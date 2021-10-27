ff50f2ed06b364c05b6f8fb3463583a249a8ffe1
MyatThiri6100 <myatthiri@ayabank.com>
2021-10-27 14:54:00 +0630

    Happy Halloween

--- src/com/aya/testing/BranchTesting.java
+++ src/com/aya/testing/BranchTesting.java
@@ -4,5 +4,6 @@ public class BranchTesting {
 	public static void main (String[]args) {
 	System.out.println("test thiri branch");
 	System.out.println("Hello");
+	System.out.println("Happy Halloween");
 	}
 }
